df081c885db9855b247701a77852d8f995906d06
FACHH23 <fachrisandms@gmail.com>
2022-09-25 10:20:58 +0700

    meengedit budidaya air

--- src/components/layout/sidebar.vue
+++ src/components/layout/sidebar.vue
@@ -469,40 +469,6 @@
                                 <li>
                                     <router-link to="/budidaya-fisika" @click="toggleMobileMenu">Parameter Fisika </router-link>
                                 </li>
-                                <li>
-                                    <router-link to="/budidaya-kimia" @click="toggleMobileMenu">Pamareter Kimia </router-link>
-                                </li>
-                                <li>
-                                    <a class="dropdown-toggle" href="#budidayaBiologi" data-bs-parent="#budidayaBiologi" data-bs-toggle="collapse" role="button" aria-expanded="false">
-                                        Parameter Biologi
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="13"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            stroke-width="2"
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            class="feather feather-chevron-right"
-                                        >
-                                            <polyline points="9 18 15 12 9 6"></polyline>
-                                        </svg>
-                                    </a>
-
-                                    <ul id="budidayaBiologi" class="mx-3 collapse list-unstyled sub-submenu">
-                                        <li>
-                                            <router-link to="/parameter-biologi-fitoplankton" @click="toggleMobileMenu"> Filoplankton </router-link>
-                                        </li>
-                                        <li>
-                                            <router-link to="/parameter-biologi-vibrioTCBS" @click="toggleMobileMenu"> Vibrio TCBS </router-link>
-                                        </li>
-                                        <li>
-                                            <router-link to="/parameter-biologi-vibrioCHROMagar" @click="toggleMobileMenu">Vibrio CHROMagar </router-link>
-                                        </li>
-                                    </ul>
-                                </li>
                             </ul>
                         </li>
                     </ul>
