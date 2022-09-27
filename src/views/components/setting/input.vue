e5e4dc9d7e0e876d419c71a87600581a1c9bb0c5
keysar07 <akunpubg1703@gmail.com>
2022-09-27 17:10:22 +0700

    Menambah setting

--- src/views/components/setting/input.vue
+++ src/views/components/setting/input.vue
@@ -6,7 +6,7 @@
                     <div class="page-header">
                         <nav class="breadcrumb-one" aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item active" aria-current="page"><span>Reminder</span></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Setting</span></li>
                             </ol>
                         </nav>
                     </div>
@@ -15,12 +15,12 @@
         </teleport>
 
         <div class="container">
-            <div id="reminder" class="col-lg-12">
+            <div id="setting" class="col-lg-12">
                 <div class="statbox panel box box-shadow">
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                                <h4>Reminder</h4>
+                                <h4>Setting</h4>
                             </div>
                         </div>
                     </div>
@@ -28,9 +28,14 @@
                         <form class="form-vertical" action="#">
                             <div class="form-group">
                                 <label class="control-label">Id:</label>
-                                <input type="text" name="id" class="form-control" />
+                                <input type="hidden" name="id" class="form-control" />
                             </div>
-
+                            <div class="form-group">
+                                <label class="control-label">Kategori:</label>
+                                <multiselect v-model="inputs['input3']" :options="options1" :searchable="true"
+                                    placeholder="Choose..." selected-label="" select-label="" deselect-label="">
+                                </multiselect>
+                            </div>
                             <div class="form-group">
                                 <label class="control-label">Tanggal:</label>
                                 <div class="form-group mb-0">
@@ -73,44 +78,44 @@
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue';
-    import '@/assets/sass/scrollspyNav.scss';
-    import '@/assets/sass/forms/file-upload-with-preview.min.css';
-    import '@suadelabs/vue3-multiselect/dist/vue3-multiselect.css';
-    import '@/assets/sass/forms/file-upload-with-preview.min.css';
-    import highlight from '@/components/plugins/highlight.vue';
+import { onMounted, ref } from 'vue';
+import '@/assets/sass/scrollspyNav.scss';
+import '@/assets/sass/forms/file-upload-with-preview.min.css';
+import '@suadelabs/vue3-multiselect/dist/vue3-multiselect.css';
+import '@/assets/sass/forms/file-upload-with-preview.min.css';
+import highlight from '@/components/plugins/highlight.vue';
 
-    import flatPickr from 'vue-flatpickr-component';
-    import 'flatpickr/dist/flatpickr.css';
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
 
-    import Multiselect from '@suadelabs/vue3-multiselect';
+import Multiselect from '@suadelabs/vue3-multiselect';
 
-    import FileUploadWithPreview from 'file-upload-with-preview';
+import FileUploadWithPreview from 'file-upload-with-preview';
 
-    import { useMeta } from '@/composables/use-meta';
-    useMeta({ title: 'File Upload' });
+import { useMeta } from '@/composables/use-meta';
+useMeta({ title: 'File Upload' });
 
-    const code_arr = ref([]);
-    const selected_file = ref(null);
+const code_arr = ref([]);
+const selected_file = ref(null);
 
-    const toggleCode = (name) => {
-        if (code_arr.value.includes(name)) {
-            code_arr.value = code_arr.value.filter((d) => d != name);
-        } else {
-            code_arr.value.push(name);
-        }
-    };
+const toggleCode = (name) => {
+    if (code_arr.value.includes(name)) {
+        code_arr.value = code_arr.value.filter((d) => d != name);
+    } else {
+        code_arr.value.push(name);
+    }
+};
 
-    const options1 = ref(['pompa', 'Kincir', 'Auto feeder', 'Alat laboratorium dan Pengukuran']);
-    const inputs = ref({
-        input1: [],
-        input2: [],
-        input3: [],
-        input4: [],
-        input5: [],
-    });
+const options1 = ref(['pompa', 'Kincir', 'Auto feeder', 'Alat laboratorium dan Pengukuran']);
+const inputs = ref({
+    input1: [],
+    input2: [],
+    input3: [],
+    input4: [],
+    input5: [],
+});
 
-    const change_file = (event) => {
-        selected_file.value = URL.createObjectURL(event.target.files[0]);
-    };
+const change_file = (event) => {
+    selected_file.value = URL.createObjectURL(event.target.files[0]);
+};
 </script>
