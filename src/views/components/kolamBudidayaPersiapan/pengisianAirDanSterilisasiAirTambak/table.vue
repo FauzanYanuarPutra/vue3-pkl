bf9b8658450913d6dff7f5b1573bc8014b0b7ed6
Biiganteng <abiyasaputra999@gmail.com>
2022-09-28 16:52:57 +0700

    menambahkan color

--- src/views/components/kolamBudidayaPersiapan/pengisianAirDanSterilisasiAirTambak/table.vue
+++ src/views/components/kolamBudidayaPersiapan/pengisianAirDanSterilisasiAirTambak/table.vue
@@ -28,7 +28,7 @@
                         </div>
                         <div class=" d-flex flex-row-reverse position-relative">
                             <router-link to="/pengisian-air-dan-sterilisasi-air-tambak/create" @click="toggleMobileMenu"
-                                class="btn btn-primary mb-2 mx-3 tambah-data">Tambah Data</router-link>
+                                class="btn mb-2 mx-3 tambah-data">Tambah Data</router-link>
                         </div>
                         <v-client-table :data="items" :columns="columns" :options="table_option">
                             <template #actions="props">
