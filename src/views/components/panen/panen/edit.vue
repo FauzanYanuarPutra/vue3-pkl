d1f015c3f5c50d7af75767511143abb00a12e432
fauzan <muhzannzs@gmail.com>
2022-09-28 14:43:47 +0700

    edit

--- src/views/components/panen/panen/edit.vue
+++ src/views/components/panen/panen/edit.vue
@@ -26,7 +26,9 @@
                     </div>
                     <div class="panel-body">
                         <form class="form-vertical" action="#">
-                            <div class="form-group">
+                        <div class="box-wrap">
+                            <div class="wrap">
+                                <div class="form-group">
                                 <label class="control-label">Id kolam:</label>
                                 <input type="id" name="id_kolam" class="form-control" />
                             </div>
@@ -36,22 +38,32 @@
                             <div class="form-group mb-4">
                                 <flat-pickr v-model="params.invoice_date" class="form-control form-control-sm flatpickr active" placeholder="Invoice Date"></flat-pickr>
                             </div>
-                            <div class="mb-3">
-                                <label for="select" class="col-form-label">Tipe panen:</label>
-                                <div>
-                                    <select id="select" class="form-select">
-                                        <option value="1">Selection, only can choose one</option>
-                                        <option value="2">Parsial</option>
-                                        <option value="3">Full</option>
-                                    </select>
-                                </div>
+                            </div>
+
+                            <div class="wrap">
+                                
+                            <div class="form-group">
+                                <label class="control-label">Tipe panen:</label>
+                                <multiselect
+                                    v-model="inputs['input1']"
+                                    :options="options1"
+                                    :searchable="true"
+                                    placeholder="Choose..."
+                                    selected-label=""
+                                    select-label=""
+                                    deselect-label=""
+                                ></multiselect>
                             </div>
 
                             <div class="form-group">
                                 <label class="control-label">DOC (Day of cultivation):</label>
                                 <input type="number" name="doc" class="form-control" />
                             </div>
-                            <br />
+                            </div>
+                        </div>
+                            
+                            
+
 
                             <input type="submit" value="Submit" class="btn btn-primary mt-3" />
                         </form>
@@ -61,18 +73,49 @@
         </div>
     </div>
 </template>
-
 <script setup>
     import { onMounted, ref } from 'vue';
-    import '@/assets/sass/apps/invoice-add.scss';
+    import '@/assets/sass/scrollspyNav.scss';
+    import '@/assets/sass/forms/file-upload-with-preview.min.css';
+    import '@suadelabs/vue3-multiselect/dist/vue3-multiselect.css';
+    import '@/assets/sass/forms/file-upload-with-preview.min.css';
+    import highlight from '@/components/plugins/highlight.vue';
 
-    //flatpickr
     import flatPickr from 'vue-flatpickr-component';
     import 'flatpickr/dist/flatpickr.css';
-    import '@/assets/sass/forms/custom-flatpickr.css';
+
+    import Multiselect from '@suadelabs/vue3-multiselect';
+
+    import FileUploadWithPreview from 'file-upload-with-preview';
 
     import { useMeta } from '@/composables/use-meta';
-    useMeta({ title: 'Invoice Add' });
+    useMeta({ title: 'File Upload' });
+
+    const options1 = ref(['Parsial', 'Full']);
+    const inputs = ref({
+        input1: [],
+        input2: [],
+        input3: [],
+        input4: [],
+        input5: [],
+    });
+    const options2 = ref(['Laki Laki', 'Perempuan']);
+    const options3 = ref(['Menikah', 'Belum menikah']);
+    const options4 = ref(['Aktiv', 'Tidak aktiv']);
+
+    const code_arr = ref([]);
+    const form1 = ref({ name: '' });
+    const is_submit_form1 = ref(false);
+    const form2 = ref({ email: '' });
+    const is_submit_form2 = ref(false);
+    const form3 = ref({ select: '' });
+    const is_submit_form3 = ref(false);
+    const form4 = ref({ first_name: 'Shaun', last_name: 'Park', username: '', city: '', state: '', zip: '', is_terms: false });
+    const is_submit_form4 = ref(false);
+    const form5 = ref({ first_name: 'Shaun', last_name: 'Park', username: '', city: '', state: '', zip: '', is_terms: false });
+    const is_submit_form5 = ref(false);
+    const form6 = ref({ first_name: 'Shaun', last_name: 'Park', username: '', city: '', state: '', zip: '', is_terms: false });
+    const is_submit_form6 = ref(false);
 
     const items = ref([]);
     const selected_file = ref(null);
@@ -102,6 +145,25 @@
         params.value.invoice_date = JSON.parse(JSON.stringify(dt));
         dt.setDate(dt.getDate() + 5);
         params.value.due_date = dt;
+
+        //currency list
+        currency_list.value = [
+            { key: 'USD - US Dollar', thumb: 'flags/en.png' },
+            { key: 'GBP - British Pound', thumb: 'flags/gbp.png' },
+            { key: 'IDR - Indonesian Rupiah', thumb: 'flags/idr.png' },
+            { key: 'INR - Indian Rupee', thumb: 'flags/inr.png' },
+            { key: 'BRL - Brazilian Real', thumb: 'flags/brl.png' },
+            { key: 'EUR - Germany (Euro)', thumb: 'flags/de.png' },
+            { key: 'TRY - Turkish Lira', thumb: 'flags/tr.png' },
+        ];
+
+        //tax type list
+        tax_type_list.value = [
+            { key: 'Deducted', value: 10 },
+            { key: 'Per Item', value: 5 },
+            { key: 'On Total', value: 25 },
+            { key: 'None', value: null },
+        ];
 
         //discount list
         discount_list.value = [
