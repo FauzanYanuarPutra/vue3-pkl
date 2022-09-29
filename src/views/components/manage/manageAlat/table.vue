20bcbb11243e92ff5206407e06d9768cb45ed723
FACHH23 <fachrisandms@gmail.com>
2022-09-29 15:31:23 +0700

    bug

--- src/views/components/manage/manageAlat/table.vue
+++ src/views/components/manage/manageAlat/table.vue
@@ -26,18 +26,25 @@
                             </div>
                         </div>
                         <div class="d-flex flex-row-reverse position-relative div-tambah">
-                            <router-link to="/manage-alat/create" @click="toggleMobileMenu"
-                                class="btn mb-2 mx-3 tambah-data">Tambah Data</router-link>
+                            <router-link to="/manage-alat/create" @click="toggleMobileMenu" class="btn mb-2 mx-3 tambah-data">Tambah Data</router-link>
                         </div>
                         <v-client-table :data="items" :columns="columns" :options="table_option">
                             <template #actions="props">
                                 <div class="table-controls d-flex no-wrap justify-content-center">
                                     <div class="px-2">
                                         <div @click="view_row(props.row)" style="cursor: pointer">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                                stroke-linecap="round" stroke-linejoin="round"
-                                                class="feather feather-eye">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                stroke-width="2"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                class="feather feather-eye"
+                                            >
                                                 <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                                 <circle cx="12" cy="12" r="3"></circle>
                                             </svg>
@@ -45,25 +52,38 @@
                                     </div>
                                     <div class="px-2">
                                         <router-link to="/manage-alat/edit" data-bs-toggle="tooltip" title="Edit">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                                stroke-linecap="round" stroke-linejoin="round"
-                                                class="feather feather-edit-2">
-                                                <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
-                                                </path>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                stroke-width="2"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                class="feather feather-edit-2"
+                                            >
+                                                <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                                             </svg>
                                         </router-link>
                                     </div>
                                     <div class="px-2">
                                         <a href="javascript:void(0);" data-bs-toggle="tooltip" title="Delete">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                                stroke-linecap="round" stroke-linejoin="round"
-                                                class="feather feather-trash-2">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                stroke-width="2"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                class="feather feather-trash-2"
+                                            >
                                                 <polyline points="3 6 5 6 21 6"></polyline>
-                                                <path
-                                                    d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                </path>
+                                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                                                 <line x1="10" y1="11" x2="10" y2="17"></line>
                                                 <line x1="14" y1="11" x2="14" y2="17"></line>
                                             </svg>
@@ -81,10 +101,10 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+    import { onMounted, ref } from 'vue';
 
-import { useMeta } from '@/composables/use-meta';
-useMeta({ title: 'Default Order Sorting Table' });
+    import { useMeta } from '@/composables/use-meta';
+    useMeta({ title: 'Default Order Sorting Table' });
 
     const columns = ref(['nama_tambak', 'tanggal_input', 'tipe_model', 'jumlah_unit', 'status', 'actions']);
     const items = ref([]);
@@ -366,5 +386,5 @@ useMeta({ title: 'Default Order Sorting Table' });
             padding: '2em',
         });
         // alert(' Name: ' + item.nama + ', MIN: ' + item.min + ', MAX: ' + item.max);
->>>>>>> d94c0b5a74c2e53eb37dde8d5947d75c9219bfb1
+    };
 </script>
